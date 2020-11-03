2f4635b0bcdb6afd49afd8c2edfb4da00c89b6ea
VIDHI3030 <72209528+VIDHI3030@users.noreply.github.com>
2020-11-03 16:43:02 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
@@ -34,9 +35,9 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
-
+    log6 = new Log(220,100,80,PI/2);
     bird = new Bird(100,100);
-
+    slingshot = new Slingshot(bird.body,log6.body);
 }
 
 function draw(){
@@ -50,7 +51,8 @@ function draw(){
     ground.display();
     pig1.display();
     log1.display();
-
+    slingshot.display();
+    log6.display();
     box3.display();
     box4.display();
     pig3.display();
