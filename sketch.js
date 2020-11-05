4adcdf6118bfb68f3e167f80f3a4f3eccbfe4144
VIDHI3030 <72209528+VIDHI3030@users.noreply.github.com>
2020-11-05 17:36:08 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -5,7 +5,8 @@ const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
-var backgroundImg;
+var backgroundImg,platform;
+var bird, slingShot;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -16,10 +17,9 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-    platform= new Ground(150,305,300,170);
 
-    
-    ground = new Ground(600,height,1200,20)
+    ground = new Ground(600,height,1200,20);
+    platform = new Ground(150, 305, 300, 170);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -35,35 +35,43 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
-    bird = new Bird(200,100);
-    slingshot = new Slingshot(bird.body,{x:200,y:100});
+
+    bird = new Bird(200,50);
+
+    //log6 = new Log(230,180,80, PI/2);
+    slingshot = new SlingShot(bird.body,{x:200, y:50});
 }
 
 function draw(){
     background(backgroundImg);
     Engine.update(engine);
-    console.log(box2.body.position.x);
-    console.log(box2.body.position.y);
-    console.log(box2.body.angle);
+    strokeWeight(4);
     box1.display();
     box2.display();
     ground.display();
     pig1.display();
     log1.display();
-    slingshot.display();
+
     box3.display();
     box4.display();
     pig3.display();
     log3.display();
+
     box5.display();
     log4.display();
     log5.display();
+
     bird.display();
-    platform.display ();
+    platform.display();
+    //log6.display();
+    slingshot.display();    
 }
+
 function mouseDragged(){
-    Matter.Body.setPosition(bird.body,{x:mouseX,y:mouseY})
+    Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
 }
+
+
 function mouseReleased(){
     slingshot.fly();
 }
