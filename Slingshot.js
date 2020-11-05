4deffb8f3c00ebc44431fee5a3685133b0ad5fed
VIDHI3030 <72209528+VIDHI3030@users.noreply.github.com>
2020-11-05 18:01:30 +0530

    Add files via upload

--- Slingshot.js
+++ Slingshot.js
@@ -19,8 +19,8 @@ class SlingShot{
     }
 
     display(){
-        image(this.sling1,200,20);
-        image(this.sling2,170,20);
+        image(this.sling1,200,225);
+        image(this.sling2,170,225);
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
--- sketch.js
+++ sketch.js
@@ -13,33 +13,33 @@ function preload() {
 }
 
 function setup(){
-    var canvas = createCanvas(1200,400);
+    var canvas = createCanvas(1200,600);
     engine = Engine.create();
     world = engine.world;
 
 
     ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
+    platform = new Ground(150, 505, 300, 170);
+    platform1= new Ground(900,300,400,20);
+    box1 = new Box(700,520,70,70);
+    box2 = new Box(920,520,70,70);
+    pig1 = new Pig(810, 550);
+    log1 = new Log(810,460,300, PI/2);
 
-    box1 = new Box(700,320,70,70);
-    box2 = new Box(920,320,70,70);
-    pig1 = new Pig(810, 350);
-    log1 = new Log(810,260,300, PI/2);
+    box3 = new Box(700,440,70,70);
+    box4 = new Box(920,440,70,70);
+    pig3 = new Pig(810, 420);
 
-    box3 = new Box(700,240,70,70);
-    box4 = new Box(920,240,70,70);
-    pig3 = new Pig(810, 220);
+    log3 =  new Log(810,380,300, PI/2);
 
-    log3 =  new Log(810,180,300, PI/2);
-
-    box5 = new Box(810,160,70,70);
-    log4 = new Log(760,120,150, PI/7);
-    log5 = new Log(870,120,150, -PI/7);
-
-    bird = new Bird(200,50);
+    box5 = new Box(810,270,70,70);
+    log4 = new Log(920,190,300, PI/2);
+    box6 = new Box(1030,270,70,70);
+    pig4 = new Pig(920,260);
+    bird = new Bird(200,250);
 
     //log6 = new Log(230,180,80, PI/2);
-    slingshot = new SlingShot(bird.body,{x:200, y:50});
+    slingshot = new SlingShot(bird.body,{x:200, y:250});
 }
 
 function draw(){
@@ -51,15 +51,15 @@ function draw(){
     ground.display();
     pig1.display();
     log1.display();
-
+    platform1.display();
     box3.display();
     box4.display();
     pig3.display();
     log3.display();
-
+    pig4.display();
     box5.display();
     log4.display();
-    log5.display();
+    box6.display();
 
     bird.display();
     platform.display();
